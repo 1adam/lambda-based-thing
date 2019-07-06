58c2e9aa5e1995711cc19e6d52a767a52f14070e
1adam <1adam@users.noreply.github.com>
2019-07-06 14:43:16 +0000

    upd w remote backend cfg

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = ""
-    key = ""
-    region = ""
+    bucket = "tf-s-dev-20190706142601220600000001"
+    key = "state/current.tfstate"
+    region = "us-east-1"
     encrypt = true
     acl = "private"
-    kms_key_id = ""
-    dynamodb_table = ""
+    kms_key_id = "arn:aws:kms:us-east-1:394737488529:key/c636b409-c6df-4d8a-9f23-c04375a7a41d"
+    dynamodb_table = "tf-s-dev-locking"
    }
 }
