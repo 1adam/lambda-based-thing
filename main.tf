dd19e109a3f625e631084d3e9f5d0295cbeb18c7
1adam <1adam@users.noreply.github.com>
2019-07-07 01:52:56 -0400

    openapi dec

--- main.tf
+++ main.tf
@@ -60,3 +60,85 @@ resource "aws_dynamodb_table" "users" {
  }
 }
 
+resource "aws_api_gateway_rest_api" "main" {
+  name = "xg-${terraform.workspace}-api"
+  description = "xg api"
+  body = <<END_API
+openapi: 3.0.2
+info:
+  title: "xg-api-app"
+  version: 1.0.1
+components:
+  schemas:
+    successfulComm:
+      type: object
+      properties:
+        cid:
+          type: string
+          format: uuid
+        uid:
+          type: string
+          format: uuid
+    err:
+      type: object
+      properties:
+        errCode:
+          type: integer
+          format: int32
+        errMsg:
+          type: string
+  parameters:
+    cid:
+      - name: cid
+        in: path
+
+paths:
+  /hellothere/{uname}:
+    parameters:
+      - name: uname
+        in: path
+        required: true
+        description: "userName"
+        schema:
+          type: string
+    post:
+      summary: conn to xg
+      description: Connect to xg
+      responses:
+        '200':
+          description: Successful conn
+          content:
+            '*/*':
+              type: object
+              schema:
+                $ref: "#/components/schemas/cid"
+  /conn/{cid}:
+    parameters:
+      - name: cid
+        in: path
+        required: true
+        description: "connID"
+        schema:
+          type: string
+    post:
+      summary: Comm w xg
+      description: Communicate with xg using existing connID
+      responses:
+        '200':
+          description: Successful comm
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/successfulComm"
+        default:
+          description: "error happen"
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/err"
+
+
+
+END_API
+
+}
