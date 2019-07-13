6d29a6ccae9a15b0dab5575ec00e29a33d959395
1adam <1adam@users.noreply.github.com>
2019-07-13 10:30:03 -0400

    defaults

--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "conn_obj_zipfile" {
   type = "string"
+  default = "conn-obj-func.zip"
 }
 
 variable "disconn_obj_zipfile" {
   type = "string"
+  default = "disconn-obj-func.zip"
 }
 
 variable "act_obj_zipfile" {
   type = "string"
+  default = "act-obj-func.zip"
 }
